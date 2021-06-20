bde940de80c7f548a59d7dc82f4f8a41f76dacaa
Guilherme <g.vieira@kroton.com.br>
2021-06-19 23:12:37 -0300

    "project"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React,{ useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header';
+import './App.css'
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setfeatureData] = useState(null);
+  const [blackHeader,setblackHeader] = useState(false)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //trazer a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+
+      //pegar o filme em destaque
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length) - 1)
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      console.log(chosenInfo)
+      setfeatureData(chosenInfo);
+    }
+    loadAll();
+  },[]);
+  
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setblackHeader(true)
+      } else {
+        setblackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header black={blackHeader}/>
+      {featureData &&
+        <FeaturedMovie item={featureData}/>
+      }
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="http://cdn.lowgif.com/full/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="Carregando"/>
+      </div>
+      }
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './Header.css'
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://crm7.com.br/wp-content/uploads/2020/11/Netflix-Logo.png" alt="Netflix"/>
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Usuário"/>
+                </a>
+            </div>
+        </header>
+    )
+}
+
+// upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png
